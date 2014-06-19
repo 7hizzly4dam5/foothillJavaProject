de0b1775a0b1476f3c79c0dc611a3aa152ab0433
7hizzly4dam5 <thomas.s.blake@gmail.com>
2014-06-19 11:17:08 -0700

    updated ContactList
    added zipSearch lastSearch emailSearch and arrays to hold each

--- contactList.java
+++ contactList.java
@@ -1,52 +1,70 @@
-/**
- * Creats an arraylist to store new contacts.
- */
-import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
 
 public class ContactList //TB, OC
 {
+   Scanner scan =new Scanner(System.in);
    
-   private ArrayList<Contact> list;  // this data member holds a contact array
+   private final int CONTACT_TOTAL = 20;
+   
+   private Contact[] list;  
+   private int count;  
+                                   
+                                   
+   private Contact[] lMatch;
+   private int lcount;
+   
+   private Contact[] eMatch;
+   private int ecount;
+   
+   private Contact[] zMatch;
+   private int zcount;
   
    /**
     * Default constructor for ContactList.  Creates an arraylist of Contacts.
     */
    ContactList()
    {
-      list = new ArrayList<Contact>();
+      list = new Contact[CONTACT_TOTAL];
+      lMatch = new Contact[CONTACT_TOTAL];
+      eMatch = new Contact[CONTACT_TOTAL];
+      zMatch = new Contact[CONTACT_TOTAL];
+      count = 0;
+      lcount = 0;
+      ecount = 0;
+      zcount = 0;
    }
    
+   /**
+    * This method returns the current length of the ContactList array list
+    * */
+   
+   public int getCount()
+   {
+      return count;
+   }
+   
+   /**
+    * This method returns the list of Contacts
+    * */
+   public Contact[] getList()
+   {
+      return list;
+   }
    /**
     * This method adds an object of type Contact to the list that calls it.
     */
    public void addContact(Contact newPerson)
    {
-      if (newPerson.getLast().isEmpty() == true)
-      {
-         newPerson = null;
-      }
-      else
-      {
-         /**
+      /**
           * The Contact argument is added to the list at the location of count
           * , and then count moved to the next open spot
           */
-         list.add(newPerson);
+         list[count] = newPerson;   
+         count+=1;               
       }
    }
    
-
-    
-   public void searchByLastName()
-   {
-      System.out.println("Please enter last name of person you are searching for")
-      String search = scanner.nextLine();
-      for int i = 0; i < list.length(); i++) 
-      {
-         if (search == list[i].getLast())
-         System.out.println(list[i])
-      }
-   }
     /**
     * This method prints all of the contacts stored in the list by calling the
     * toString() method for that Contact object
@@ -59,4 +77,91 @@ public class ContactList //TB, OC
       }
       
    }
+   
+    public void lastAccess()
+   {
+      System.out.println("\nEnter a last name address to search:");
+      
+      //creates Scanner object to accept input, assigns to String search
+      Scanner scan = new Scanner(System.in);
+      String search;
+      search = new String(scan.next());
+      
+      //adds all matching las names to the lMatch contact array
+      for (int i = 0; i < count; i++)
+      {
+         String lastCheck;
+         lastCheck = list[i].getLast();  //current last name is the contactsearc
+         
+         if (search.equals(lastCheck))
+         {
+            lMatch[lcount] = list[i];
+            lcount+=1;
+         }
+      }
+      
+      //print all of the contacts with matching last names
+      for (int i = 0; i < lcount; i++)
+      {
+         System.out.println(lMatch[i].toString());
+      }
+   }
+   
+   public void emailAccess()
+   {
+      
+      System.out.println("\nEnter an email address to search:");
+      //creates Scanner object to accept input, assigns to String search
+      Scanner scan = new Scanner(System.in);
+      String search;
+      search = new String(scan.next());
+      
+      //adds all matching las names to the lMatch contact array
+      for (int i = 0; i < count; i++)
+      {
+         String emailCheck;
+         emailCheck = list[i].getEmail();  //current last name is the contactsearc
+         
+         if (search.equals(emailCheck))
+         {
+            eMatch[ecount] = list[i];
+            ecount+=1;
+         }
+      }
+      
+      //print all of the contacts with matching last names
+      for (int i = 0; i < ecount; i++)
+      {
+         System.out.println(eMatch[i].toString());
+      }
+   }
+   
+   public void zipAccess()
+   {
+      System.out.println("\nEnter a Zipcode to search:");
+      //creates Scanner object to accept input, assigns to String search
+      Scanner scan = new Scanner(System.in);
+      String search;
+      search = new String(scan.next());
+      
+      //adds all matching las names to the lMatch contact array
+      for (int i = 0; i < count; i++)
+      {
+         String zipCheck;
+         zipCheck = list[i].getZip();  //current last name is the contactsearc
+         
+         if (search.equals(zipCheck))
+         {
+            zMatch[zcount] = list[i];
+            zcount+=1;
+         }
+      }
+      
+      //print all of the contacts with matching last names
+      for (int i = 0; i < zcount; i++)
+      {
+         System.out.println(zMatch[i].toString());
+      }
+   }
+}
 }
